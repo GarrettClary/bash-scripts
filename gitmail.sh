1a2b6aa4b80f57101bbe9bdaa7dc0b28f611db70
Garrett Clary <claryg0603@protonmail.com>
2018-11-18 22:06:54 -0800

    First push of gitmail.sh and edited README

---
+++ gitmail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git_usr="$1"
+out_file="$2"
+
+script="$(basename -- "$0")"
+
+usage() {
+    echo -e "usage: "$script" [git_user] [out_file]"    
+}
+
+if [ "$#" -ne 2 ]; then
+    usage
+else
+    wget -O "$out_file" https://api.github.com/users/"$git_usr"/events/public &> /dev/null
+    grep email "$out_file" | sort -u
+fi
